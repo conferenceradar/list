b35063c036b9b58f147d6d0105022e73df16f97f
Ryan Lanciaux <ryan.lanciaux@gmail.com>
2017-09-08 12:38:36 -0400

    Updated twitter links

--- src/App.js
+++ src/App.js
@@ -378,7 +378,7 @@ class App extends Component {
       <Footer>
         <FooterLeft>
           <p>
-            The data is based on conference lists from Heather Wilde and Cory House.
+            The data is based on conference lists from <a href="https://twitter.com/heathriel">Heather Wilde</a> and <a href="https://twitter.com/housecor">Cory House</a>.
           </p>
           <p>
             Site compiled by <a href="https://twitter.com/ryanlanciaux">Ryan Lanciaux</a> using <a href="https://github.com/facebookincubator/create-react-app">Create React App</a> with <a href="http://griddlegriddle.github.io/Griddle">Griddle</a>
