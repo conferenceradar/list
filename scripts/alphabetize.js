f0153d561ccf77afa18a72c0e365e03f5eadc115
Ryan Lanciaux <ryan.lanciaux@gmail.com>
2017-09-11 20:02:13 -0400

    Update alphabetize

--- scripts/alphabetize.js
+++ scripts/alphabetize.js
@@ -1,14 +1,30 @@
 var events = require('../src/events.json');
 var fs = require('fs')
 
+function getLocationString (item) {
+  return item.country + '-' + item.stateProvince + '-' + item.city;
+}
+
 events.sort(function(a, b) {
   var nameA = a.name.toUpperCase(); // ignore upper and lowercase
   var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+
+  var locationA = getLocationString(a);
+  var locationB = getLocationString(b);
+
   if (nameA < nameB) {
     return -1;
   }
   if (nameA > nameB) {
     return 1;
+  }
+
+  //items have same name but check location
+  if (locationA < locationB) {
+    return -1;
+  }
+  if (locationA > locationB) {
+    return 1
   }
 
   // names must be equal
