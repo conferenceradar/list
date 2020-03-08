55e37d7c68bdd0bd337eeaebc0a46b29c8645f4a
Ryan Lanciaux <ryan.lanciaux@gmail.com>
2020-03-08 09:56:40 -0400

    Updates mobile views with status

--- src/DetailsSection/CardList/styles.js
+++ src/DetailsSection/CardList/styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const CardWrapper = styled.div`
   position: relative;
@@ -8,16 +8,15 @@ export const CardWrapper = styled.div`
     font-weight: bold;
     font-size: 16px;
   }
-`
+`;
 
-export const ListWrapper = styled.div`
-`
+export const ListWrapper = styled.div``;
 
 export const CenteredPagination = styled.div`
   width: 100%;
   text-align: center;
   margin: 25px 0 25px 0;
-`
+`;
 
 export const GriddleWrapper = styled.div`
   margin-bottom: 120px;
@@ -31,28 +30,23 @@ export const TopWrapper = styled.div`
   display: flex;
   margin: 10px;
 
-  @media(max-width: 640px) {
+  @media (max-width: 640px) {
     button {
       display: none;
     }
   }
-
-`
+`;
 
 export const Left = styled.div`
   width: 50%;
-`
+`;
 export const Right = styled.div`
   width: 50%;
   text-align: right;
   padding-right: 20px;
-`
+`;
 
 export const DateWrapper = styled.div`
-  position: absolute;
-  right: 10px;
-  top: 20px;
-
   color: #999;
 
   small {
